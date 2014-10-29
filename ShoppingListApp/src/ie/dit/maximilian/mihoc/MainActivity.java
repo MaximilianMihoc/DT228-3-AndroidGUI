a2a4441935b14f0d3c4ecd96dbd514a7e6b52185
MaximilianMihoc <Maximilian@Mihoc>
2014-10-29 01:12:18 +0000

    Starting the Entry Screen

--- ShoppingListApp/src/ie/dit/maximilian/mihoc/MainActivity.java
+++ ShoppingListApp/src/ie/dit/maximilian/mihoc/MainActivity.java
@@ -2,31 +2,58 @@ package ie.dit.maximilian.mihoc;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity 
+{
+	TextView title;
+	Spinner jobTitle;
+	EditText ageValue;
+	EditText totalValue;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		title = (TextView)findViewById(R.id.maintitle);
+		title.setGravity(Gravity.CENTER);
+		
+		jobTitle = (Spinner)findViewById(R.id.jobTytle);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.job_titles, android.R.layout.simple_spinner_dropdown_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		jobTitle.setAdapter(adapter);
+		
+		ageValue = (EditText)findViewById(R.id.ageValue);
+		ageValue.setGravity(Gravity.RIGHT);
+		totalValue = (EditText)findViewById(R.id.totalValue);
+		totalValue.setGravity(Gravity.RIGHT);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_settings) 
+		{
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
